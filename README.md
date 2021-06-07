7ce2e533a471e491e68d737b29df2353dced0538
cryswerton <cryswertons@gmail.com>
2021-06-07 01:48:20 -0300

    added README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+Um projeto simples que usa o JSON-SERVER como banco de dados.
+
+Requisitos para uso:
+    Instalar o plugin Live Server no VS Code;
+    instalar o json-server
+
+Como usar:
+    Clique com o botão direito do mouse em cima do seu arquivo index.html
+    e selecione a opção "Abrir com live server";
+
+    No terminal, detro da sua pasta do projeto use o comando:
+        npm run start
+    
+    Agora que o servidor json foi iniciado você já pode adicionar e deletar
+    posts ao acessar a página que abriu ao iniciar o Live Server.
--- js/details.js
+++ js/details.js
@@ -1,6 +1,7 @@
 // javascript for details.html
 const id = new URLSearchParams(window.location.search).get('id')
 const container = document.querySelector('.details')
+const deleteBtn = document.querySelector('.delete')
 
 const renderDetails = async () => {
     const res = await fetch('http://localhost:3000/books/' + id)
@@ -16,6 +17,13 @@ const renderDetails = async () => {
     container.innerHTML = template
 
 }
+
+deleteBtn.addEventListener('click', async (e) => {
+    const res = await fetch('http://localhost:3000/books/' + id, {
+        method: 'DELETE'
+    })
+    window.location.replace('/index.html')
+})
 
 window.addEventListener('DOMContentLoaded', () => renderDetails())
 
